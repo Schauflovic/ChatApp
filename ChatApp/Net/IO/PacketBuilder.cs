20388a661f086389c9a6bbc1877c20c0ca8b47e0
Schauflovic <f.schaeufle@gmail.com>
2022-08-31 14:07:53 +0200

    Projektdateien hinzufügen.

---
+++ ChatApp/Net/IO/PacketBuilder.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatClient.Net.IO
+{
+    /*//Klasse ermöglicht uns Daten an einen MemoryStream anzuhängen, 
+     * den man verwenden kann, um die Bytes zu erhalten, 
+     * die an den Server gesendet werden sollen, 
+     * denn der Datenaustausch zwischen Client und Server passiert 
+     * nur mit Bytes
+     */
+    class PacketBuilder 
+    {
+        MemoryStream _ms;
+        public PacketBuilder()
+        {
+            _ms = new MemoryStream();
+        }
+
+        /*
+         * 3 Funktionen:
+         * 1. ermöglicht schreiben von OPCode in das Paket
+         */
+
+        public void WriteOpCode(byte opcode)
+        {
+            _ms.WriteByte(opcode);
+        }
+
+        public void WriteMessage(string msg)
+        {
+            var msgLength = msg.Length;
+            _ms.Write(BitConverter.GetBytes(msgLength));
+            _ms.Write(Encoding.ASCII.GetBytes(msg));
+        }
+
+        public byte[] GetPacketBytes()
+        {
+            return _ms.ToArray();
+        }
+    }
+}
---
+++ ChatApp/Net/Server.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using ChatClient.Net.IO;
+
+namespace ChatClient.Net
+{
+    class Server
+    {
+        TcpClient _client;
+        PacketBuilder _packetBuilder;
+        public PacketReader PacketReader;
+
+        public event Action connectedEvent;
+        public event Action msgReceivedEvent;
+        public event Action userDisconnectEvent;
+
+        public Server()
+        {
+            _client = new TcpClient();
+        }
+        
+        public void ConnectToServer(string username)
+        {
+            if (!_client.Connected) //wenn Client nicht connected ist, dann 
+            {
+                _client.Connect("127.0.0.1", 7891); //connet to a server
+                PacketReader = new PacketReader(_client.GetStream());
+
+                if (!string.IsNullOrEmpty(username))
+                {
+                    var connectPacket = new PacketBuilder();
+                    connectPacket.WriteOpCode(0);
+                    connectPacket.WriteMessage(username);
+                    _client.Client.Send(connectPacket.GetPacketBytes());
+                }
+                ReadPackets();
+            }
+        }
+
+        private void ReadPackets()
+        {
+            Task.Run(() =>
+            {
+                while (true)
+                {
+                    var opcode = PacketReader.ReadByte();
+                    switch (opcode)
+                    {
+                        case 1:
+                            connectedEvent?.Invoke();
+                            break;
+
+                        case 5:
+                            msgReceivedEvent?.Invoke();
+                            break;
+
+                        case 10:
+                            userDisconnectEvent?.Invoke();
+                            break;
+
+                        default:
+                            Console.WriteLine("ah yes ...");
+                            break;
+                    }
+                }
+            });
+        }
+
+        public void SendMessageToServer(string message)
+        {
+            var messagePacket = new PacketBuilder();
+            messagePacket.WriteMessage(message);
+            messagePacket.WriteOpCode(5);
+            _client.Client.Send(messagePacket.GetPacketBytes());
+        }
+    }
+}
